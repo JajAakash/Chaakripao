cc219f7d00cd1a6ffef2f51e7e6982e63dbe4333
unknown <aakashjaj11@gmail.com>
2020-02-17 18:57:26 +0530

    loader added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,12 +37,13 @@ export function socialConfigs() {
       },  
       {  
         id: GoogleLoginProvider.PROVIDER_ID,
+
+        //for prod enviroment google_provider_id
+        provider: new GoogleLoginProvider('265370481252-27c9mrnac121u15d9qskrg84ds6r3e0n.apps.googleusercontent.com')
         
         // for dev enviroment google_provider_id
          //provider: new GoogleLoginProvider('95434812881-nl4280nc6f0kbukd8upsv78qrikf1h9a.apps.googleusercontent.com')  
-
-         //for prod enviroment google_provider_id
-        provider: new GoogleLoginProvider('265370481252-27c9mrnac121u15d9qskrg84ds6r3e0n.apps.googleusercontent.com')  
+  
       }  
     ]  
   );  
